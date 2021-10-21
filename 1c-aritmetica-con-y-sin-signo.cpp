359a7a06cfe7be0ae9237877e644371547d185e6
Miguel Ángel Latre Abadía <latre@unizar.es>
2021-10-21 13:18:54 +0200

    Update 1c-aritmetica-con-y-sin-signo.cpp

--- 1c-aritmetica-con-y-sin-signo.cpp
+++ 1c-aritmetica-con-y-sin-signo.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 /*
- * Programa que muestra resultados de una suma y una  multiplicación utilizando
+ * Programa que muestra resultados de una suma y una multiplicación utilizando
  * enteros con y sin signo.
  */
 int main() {
@@ -21,7 +21,7 @@ int main() {
 
     // La suma se calcula como (unsigned(a) + b) % 2³² =
     // = (4294967288 + 3) % 4294967296 = 4294967291
-    cout << "Suma a + b: " << a + b << endl;
+    cout << "    Suma a + b: " << a + b << endl;
     // El producto se calcula como (unsigned(a) * b) % 2³² =
     // = (4294967288 * 3) % 4294967296 = 4294967272
     cout << "Producto a * b: " << a * b << endl;
@@ -29,7 +29,7 @@ int main() {
 
     // Si queremos obtener la suma -8 + 3 o el producto de -8 * 3, hay que
     // convertir explícitamente b en entero con signo:
-    cout << "Suma a + int(b): " << a + int(b) << endl;
+    cout << "    Suma a + int(b): " << a + int(b) << endl;
     cout << "Producto a * int(b): " << a * int(b) << endl;
 
     return 0;
