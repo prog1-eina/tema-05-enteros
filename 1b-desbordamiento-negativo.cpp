edafb47eedd62116947894a9cef6604c583e6dea
Miguel Ángel Latre Abadía <latre@unizar.es>
2022-10-14 11:34:26 +0200

    Mas desbordamientos

--- 1b-desbordamiento-negativo.cpp
+++ 1b-desbordamiento-negativo.cpp
@@ -1,17 +1,22 @@
 /*******************************************************************************
  * Curso de Programación 1. Tema 5
- * Autores: Miguel Ángel Latre
- * Última revisión: 6 de octubre de 2021
+ * Autores: Miguel Ángel Latre y Ricardo J. Rodríguez
+ * Última revisión: 14 de octubre de 2022
  * Resumen: Programa que muestra los efectos de un desbordamiento negativo.
  ******************************************************************************/
 #include <iostream>
+#include <climits>
 using namespace std;
 
 /*
  * Programa que muestra los efectos de un desbordamiento negativo.
  */
 int main() {
-    int i = 2147483647; // último entero con signo representable con 32 bits (2³¹ - 1)
+    // INT_MAX es mayor entero con signo representable como int.
+    // Está definido en <climits>
+    // Si la implementación de int usa 32 bits, es 2³¹ - 1 = 2147483647
+    int i = INT_MAX; 
+    cout << "Antes de sumar: " << i << endl;
     i++;
-    cout << i << endl;
+    cout << "Tras sumar:    "  << i << endl;
 }
---
+++ 1c-desbordamiento-negativo-unsigned.cpp
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Curso de Programación 1. Tema 5
+ * Autores: Ricardo J. Rodríguez
+ * Última revisión: 14 de octubre de 2022
+ * Resumen: Programa que muestra los efectos de un desbordamiento negativo con
+ *          datos de tipo unsigned.
+ ******************************************************************************/
+#include <iostream>
+#include <climits>
+using namespace std;
+
+/*
+ * Programa que muestra los efectos de un desbordamiento negativo con datos de tipo
+ * unsigned.
+ */
+int main() {
+    // UINT_MAX es mayor entero sin signo representable como unsigned int.
+    // Está definido en <climits>
+    // Si la implementación de unsigned int usa 32 bits, es 2³² - 1 = 4294967295
+    unsigned i = UINT_MAX; 
+    cout << "i = " << i << endl;
+    cout << "¿¿i == -1??: " << boolalpha << (i == -1) << endl;
+}
